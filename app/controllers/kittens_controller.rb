140b6b612d46b6f61fc2c8e3f8e2e0e779481828
Daniel Murphy <danielmichaelmurphy@gmail.com>
2022-04-21 14:47:35 -0400

    Add RESTful methods to KittensController

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,23 @@
 class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
